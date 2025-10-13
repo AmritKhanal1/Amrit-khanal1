4b419940d94c2b1f8b234e19e9ecb898b264cdc5
AmritKhanal1 <157819837+AmritKhanal1@users.noreply.github.com>
2025-10-13 07:10:18 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/AmritKhanal1/", // ✅ Add the trailing slash
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/Amrit-khanal1/", // 👈 Important for GitHub Pages deployment
 });
